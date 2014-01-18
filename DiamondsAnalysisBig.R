0e626aa8658971ba10d52cc2917f4905d13c33b3
solomon <solomon.messing@gmail.com>
2014-01-18 13:21:12 -0800

    Cleaning up

--- DiamondsAnalysisBig.R
+++ DiamondsAnalysisBig.R
@@ -3,9 +3,12 @@
 # Author: solomon
 ###############################################################################
 
-setwd("~/Dropbox/public/Blogposts/Diamonds")
-load( file="data/BigDiamonds.Rda")
-diamonds = diamonds[diamonds$cert=="GIA",]
+#install.packages('RCurl')
+library('RCurl')
+diamondsurl = getBinaryURL("https://raw.github.com/solomonm/diamonds-data/master/BigDiamonds.Rda")
+load(rawConnection(diamondsurl))
+
+diamondsbig = diamondsbig[diamondsbig$cert=="GIA",]
 
 #install.packages('GGally')
 #install.packages('ggplot2')
@@ -16,13 +19,13 @@ library(ggplot2)
 library(GGally)
 library(scales)
 
-diasamp = diamonds[sample(1:length(diamonds$price), 10000), 1:8]
+diasamp = diamondsbig[sample(1:length(diamondsbig$price), 10000), 1:8]
 png("plots/ggpairs.png", height=1000, width=1000)
 ggpairs(na.omit(diasamp), params = c(shape = I("."), outlier.shape = I(".")))
 dev.off()
 
 
-p = qplot(price, data=diamonds, binwidth=0.01) +
+p = qplot(price, data=diamondsbig, binwidth=0.01) +
 		theme_bw() +
 		ggtitle("Price")
 p
@@ -30,7 +33,7 @@ p
 ggsave("plots/price.png", height=5, width=5)
 
 
-p = qplot(price, data=diamonds, binwidth = 0.01) +
+p = qplot(price, data=diamondsbig, binwidth = 0.01) +
 		scale_x_log10() +
 		theme_bw() +
 		ggtitle("Price (log10)")
@@ -39,13 +42,13 @@ p
 ggsave("plots/pricelog10.png", height=5, width=5)
 
 
-p = qplot(carat, price, data=diamonds, xlim=c(0,3)) +
+p = qplot(carat, price, data=diamondsbig, xlim=c(0,3)) +
 		theme_bw() +
 		ggtitle("Price by Carat")
 ggsave("plots/caratprice.png", height=5, width=5)
 
 
-p = qplot(carat, price, data=diamonds) +
+p = qplot(carat, price, data=diamondsbig) +
 		scale_y_continuous(trans=log10_trans() ) +
 		theme_bw() +
 		ggtitle("Price (log10) by Cubed-Root of Carat")
@@ -56,7 +59,7 @@ ggsave("plots/caratpricelog10.png", height=5, width=5)
 
 cubroot_trans = function() trans_new("cubroot", transform= function(x) x^(1/3), inverse = function(x) x^3 )
 
-p = qplot(carat, price, data=diamonds) +
+p = qplot(carat, price, data=diamondsbig) +
 		scale_x_continuous(trans=cubroot_trans(), limits = c(0.2,4),
 				breaks = c(0.2, 0.5, 1, 2, 3)) +
 		scale_y_continuous(trans=log10_trans(), limits = c(350,15000),
@@ -68,12 +71,12 @@ p
 ggsave("plots/crcaratpricelog10.png", height=5, width=5)
 
 
-head(sort(table(diamonds$carat), decreasing=T ))
-head(sort(table(diamonds$price), decreasing=T ))
+head(sort(table(diamondsbig$carat), decreasing=T ))
+head(sort(table(diamondsbig$price), decreasing=T ))
 
 
 
-p = ggplot( data=diamonds, aes(carat, price)) +
+p = ggplot( data=diamondsbig, aes(carat, price)) +
 		geom_point(alpha = 0.5, size = .75, position="jitter") +
 		scale_x_continuous(trans=cubroot_trans(), limits = c(0.2,4),
 				breaks = c(0.2, 0.5, 1, 2, 3)) +
@@ -87,7 +90,7 @@ ggsave("plots/crcaratpricejitterlog10.png", height=5, width=6)
 
 
 
-p = ggplot( data=diamonds, aes(carat, price, colour=cert)) +
+p = ggplot( data=diamondsbig, aes(carat, price, colour=cert)) +
 		geom_point(alpha = 0.5, size = .75, position="jitter") +
     scale_colour_brewer(type = "div",
         guide = guide_legend(title = NULL, reverse=T,
@@ -104,7 +107,7 @@ p
 ggsave("plots/crcaratpricecertlog10.png", height=5, width=6)
 
 
-p = ggplot( data=diamonds, aes(carat, price, colour=clarity)) +
+p = ggplot( data=diamondsbig, aes(carat, price, colour=clarity)) +
 		geom_point(alpha = 0.5, size = .75, position="jitter") +
 		scale_colour_brewer(type = "div",
 				guide = guide_legend(title = NULL, reverse=T,
@@ -121,7 +124,7 @@ p
 ggsave("plots/crcaratpriceclairtylog10.png", height=5, width=6)
 
 
-#p = ggplot( data=diamonds, aes(carat, price, colour=clarity)) +
+#p = ggplot( data=diamondsbig, aes(carat, price, colour=clarity)) +
 #		geom_point(alpha = 0.5, size = .75, position="jitter") +
 #		geom_smooth(method = "loess", size = 1.5) +
 #		scale_colour_brewer(type = "div",
@@ -135,7 +138,7 @@ ggsave("plots/crcaratpriceclairtylog10.png", height=5, width=6)
 #ggsave("plots/crcaratpriceclairtylog10lowess.png", height=5, width=6)
 
 
-p = ggplot( data=diamonds, aes(carat, price, colour=cut)) +
+p = ggplot( data=diamondsbig, aes(carat, price, colour=cut)) +
 		geom_point(alpha = 0.5, size = .75, position="jitter") +
 		scale_colour_brewer(type = "div",
 				guide = guide_legend(title = NULL, reverse=T,
@@ -151,7 +154,7 @@ p
 ggsave("plots/caratpricecutlog10.png", height=5, width=6.25)
 
 
-p = ggplot( data=diamonds, aes(carat, price, colour=color)) +
+p = ggplot( data=diamondsbig, aes(carat, price, colour=color)) +
 		geom_point(alpha = 0.5, size = .75, position="jitter") +
 		scale_colour_brewer(type = "div",
 				guide = guide_legend(title = NULL, reverse=T,
@@ -167,39 +170,7 @@ p
 ggsave("plots/caratpricecolorlog10.png", height=5, width=6.25)
 
 
-#install.packages('RCurl')
-library('RCurl')
-diamondsurl = getBinaryURL("https://raw.github.com/solomonm/diamonds-data/master/BigDiamonds.Rda")
-load(rawConnection(diamondsurl))
-
-m1 = lm(I(log(price))~ I(carat^(1/3)), data=diamondsbig[diamondsbig$cert=="GIA" & diamondsbig$price < 10000,])
-m2 = update(m1, ~ . + carat)
-m3 = update(m2, ~ . + I(as.numeric(clarity)) )
-m4 = update(m3, ~ . + I(as.numeric(color)) + I(as.numeric(cut)) )
-library(memisc)
-mtable(m1, m2, m3, m4)
-
- m1 = lm(I(log(price))~  I(carat^(1/3)), data=diamonds)
- m2 = update(m1, ~ . + carat)
- m3 = update(m2, ~ . + cut )
- m4 = update(m3, ~ . + color + clarity)
-# library(memisc)
-# mtable(m1, m2, m3, m4, m5)
-
-diamondsbig = diamonds
-
 diamondsbig$logprice = log(diamondsbig$price) 
-
-m1 = lm(logprice~ I(carat^(1/3)), data=diamondsbig[diamondsbig$cert=="GIA" & diamondsbig$price < 10000,])
-m2 = update(m1, ~ . + carat)
-m3 = update(m2, ~ . + I(as.numeric(clarity)) )
-m4 = update(m3, ~ . + I(as.numeric(color)) + I(as.numeric(cut)) )
-library(memisc)
-mtable(m1, m2, m3, m4)
-
-
-
-
 
 m1 = lm(logprice~  I(carat^(1/3)), data=diamondsbig[,c(1:4, 13)])
 m2 = update(m1, ~ . + carat)
@@ -207,18 +178,6 @@ m3 = update(m2, ~ . + cut )
 m4 = update(m3, ~ . + color + clarity)
 mtable(m1, m2, m3, m4)
 
-
-
-
-#install.packages('DAAG')
-library(DAAG)
-cv2 = cv.lm(df=na.omit(diamondsbig[,c(1:4, 13)]), m2, m=10) 
-cv3 = cv.lm(df=diamondsbig[,c(1:4, 13)], m3, m=10) 
-cv4 = cv.lm(df=diamondsbig[,c(1:4, 13)], m4, m=10) 
-
-
-
-levels(cv$color)
 
 thisDiamond = data.frame(carat = .7, cut = "Ideal", color = "I", clarity="VS2")
 modEst = predict(m4, newdata = thisDiamond, interval="prediction", level = .95)
@@ -236,11 +195,12 @@ thisDiamond = data.frame(carat = 1.00)
 modEst = predict(m2, newdata = thisDiamond, interval="prediction", level = .95)
 exp(modEst)
 
-
-
-
-
-
+# This will take a while to run
+#install.packages('DAAG')
+library(DAAG)
+cv2 = cv.lm(df=na.omit(diamondsbig[,c(1:4, 13)]), m2, m=10) 
+cv3 = cv.lm(df=na.omit(diamondsbig[,c(1:4, 13)]), m3, m=10) 
+cv4 = cv.lm(df=na.omit(diamondsbig[,c(1:4, 13)]), m4, m=10) 
 
 
 
